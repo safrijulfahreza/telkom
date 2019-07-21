3446f839e7b7785b35b77cb6dbff60bcdd027fb2
rezadyk <safri.110798@gmail.com>
2019-07-21 18:09:15 +0700

    Control tampilan dari user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,7 +5,13 @@ class User extends CI_Controller
 {
     public function index()
     {
+        $data['title'] = 'My Profile';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-        echo 'Selamat datang ' . $data['user']['name'];
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('user/index', $data);
+        $this->load->view('templates/footer');
     }
 }

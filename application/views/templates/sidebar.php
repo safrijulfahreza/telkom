56242341ba60fa6ed46ff4601a08f89e44480eb7
rezadyk <safri.110798@gmail.com>
2019-07-23 13:58:24 +0700

    Add menu management

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -12,33 +12,42 @@
        <!-- Divider -->
        <hr class="sidebar-divider">
 
-       <!-- Heading -->
-       <div class="sidebar-heading">
-           Admin
-       </div>
+       <?php
+        $role_id = $this->session->userdata('role_id');
+        $queryMenu = "SELECT `user_menu`.`id`, `menu`
+                    FROM `user_menu` JOIN `user_access_menu`
+                    ON  `user_menu`.`id` = `user_access_menu`.`menu_id`
+                    WHERE `user_access_menu`.`role_id`=$role_id
+                    ORDER BY `user_access_menu`.`menu_id` ASC
+       ";
+        $menu = $this->db->query($queryMenu)->result_array();
+        ?>
 
-       <!-- Nav Item - Dashboard -->
-       <li class="nav-item">
-           <a class="nav-link" href="index.html">
-               <i class="fas fa-fw fa-tachometer-alt"></i>
-               <span>Dashboard</span></a>
-       </li>
-
-       <!-- Divider -->
-       <hr class="sidebar-divider">
-
-       <!-- Heading -->
-       <div class="sidebar-heading">
-           User
-       </div>
-
-       <li class="nav-item">
-           <a class="nav-link" href="charts.html">
-               <i class="fas fa-user fa-fw"></i>
-               <span>My Profile</span></a>
-       </li>
-
-       <hr class="sidebar-divider">
+       <!-- LOOPING MENU -->
+       <?php foreach ($menu as $m) : ?>
+           <div class="sidebar-heading">
+               <?= $m['menu']; ?>
+           </div>
+           <?php
+            $menuId = $m['id'];
+            $querySubMenu = "SELECT *
+                            FROM `user_sub_menu` JOIN `user_menu`
+                            ON `user_sub_menu`.`menu_id` = `user_menu`.`id`
+                            WHERE `user_sub_menu`.`menu_id`= $menuId
+                            AND `user_sub_menu`.`is_active`= 1
+           ";
+            $subMenu = $this->db->query($querySubMenu)->result_array();
+            ?>
+           <?php foreach ($subMenu as $sm) : ?>
+               <!-- Nav Item - Dashboard -->
+               <li class="nav-item">
+                   <a class="nav-link" href="<?= base_url($sm['url']); ?>">
+                       <i class="<?= $sm['icon']; ?>"></i>
+                       <span><?= $sm['title']; ?></span></a>
+               </li>
+           <?php endforeach; ?>
+           <hr class="sidebar-divider">
+       <?php endforeach; ?>
        <li class="nav-item">
            <a class="nav-link" href="<?= base_url('auth/logout'); ?>">
                <i class="fas fa-sign-out-alt fa-fw"></i>

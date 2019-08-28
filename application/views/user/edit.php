e7266759b0d1e0f687bb4fb9856f708e51602371
rezadyk <safri.110798@gmail.com>
2019-08-29 00:56:05 +0700

    replace a new label from email to NIK

--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -7,7 +7,7 @@
         <div class="col-lg-8">
             <?= form_open_multipart('user/edit') ?>
             <div class="form-group row">
-                <label for="email" class="col-sm-2 col-form-label">Email</label>
+                <label for="email" class="col-sm-2 col-form-label">NIK</label>
                 <div class="col-sm-10">
                     <input type="text" class="form-control" id="email" name="email" value="<?= $user['email']; ?>" readonly>
                 </div>

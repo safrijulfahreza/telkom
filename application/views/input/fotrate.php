ace543df5059a8a8a9187eef4b56847348a15773
rezadyk <safri.110798@gmail.com>
2020-06-20 15:54:34 +0700

    penambahan funsi copy button

--- application/views/input/fotrate.php
+++ application/views/input/fotrate.php
@@ -5,33 +5,33 @@
 
     <div class="row">
         <div class="col-lg-8">
-            <form>
-                <div class="form-group row">
-                    <label for="nik" class="col-sm-2 col-form-label">Nomor Tiket</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="nomor_tiket" name="nomor_tiket" value="<?= $nomor_tiket; ?>" readonly>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <div class="col-sm-2">Picture</div>
-                    <div class="col-sm-10">
-                        <div class="row">
-                            <div class="col-sm-7">
-                                <img src="<?= base_url('assets/img/laporan/') . $laporan['image']; ?>" class="img-thumbnail" alt="Responsive image">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <div class="col-sm-2">URL Rating</div>
-                    <div class="col-sm-10">
-                        <div class="row">
-                            <input type="text" class="form-control" id="url" name="url">
-                        </div>
-                    </div>
-                </div>
 
-            </form>
+            <div class="form-group row">
+                <label for="nik" class="col-sm-2 col-form-label">Nomor Tiket</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="nomor_tiket" name="nomor_tiket" value="<?= $nomor_tiket; ?>" readonly>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-2">Picture</div>
+                <div class="col-sm-10">
+                    <div class="row">
+                        <div class="col-sm-7">
+                            <img src="<?= base_url('assets/img/laporan/') . $laporan['image']; ?>" class="img-thumbnail" alt="Responsive image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-2">URL Rating</div>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="url" name="url" value="<?= base_url('/rating/') . $penilaian['token']; ?>" readonly>
+                    <button onclick="myFunction()" onmouseout="outFunc()" class="btn btn-secondary">
+                        <span class="tooltiptext" id="myTooltip">Copy to clipboard</span>
+                    </button>
+                </div>
+            </div>
+
         </div>
     </div>
 

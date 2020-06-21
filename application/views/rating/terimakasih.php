e573555753b1cbc572afaf8a044dff6ff76b6b5d
rezadyk <safri.110798@gmail.com>
2020-06-21 17:29:56 +0700

    initial commit

---
+++ application/views/rating/terimakasih.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terimakasih</title>
+
+    <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div class="card text-center">
+        <div class="card-header">
+            Featured
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Terimakasih</h5>
+            <p class="card-text">Karena Sudah Menggunakan Layanan Kami.</p>
+        </div>
+    </div>
+
+</body>
+
+</html>

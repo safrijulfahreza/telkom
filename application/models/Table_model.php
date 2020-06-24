9f8b5b6fca8f4dee0d39d930880adf1ce915007e
rezadyk <safri.110798@gmail.com>
2020-06-24 22:37:44 +0700

    penambahn query

--- application/models/Table_model.php
+++ application/models/Table_model.php
@@ -9,4 +9,22 @@ class Table_model extends CI_Model
 
         return $this->db->query($query)->result_array();
     }
+
+    public function getLaporanDitangani($name)
+    {
+        $query = "SELECT COUNT(helpdesk) AS helpdesk FROM input WHERE helpdesk = '$name'";
+        return $this->db->query($query)->row_array();
+    }
+
+    public function getLaporanClose($name)
+    {
+        $query = "SELECT COUNT(helpdesk) AS close FROM input WHERE helpdesk = '$name' AND status = 'CLOSE'";
+        return $this->db->query($query)->row_array();
+    }
+
+    public function dataGrafikStat()
+    {
+        $query = "SELECT status, COUNT(*) AS total FROM input GROUP BY status";
+        return $this->db->query($query)->result();
+    }
 }

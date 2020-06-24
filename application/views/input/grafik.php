b407efdf3f3ee89d62136512ebc7c5e10ce9398b
rezadyk <safri.110798@gmail.com>
2020-06-25 00:03:46 +0700

    penambahan text

--- application/views/input/grafik.php
+++ application/views/input/grafik.php
@@ -8,7 +8,7 @@
     <!-- Bar Chart -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Bar Gangguan</h6>
+            <h6 class="m-0 font-weight-bold text-primary"> Grafik Bar Gangguan</h6>
         </div>
         <div class="card-body">
             <div class="chart-bar">

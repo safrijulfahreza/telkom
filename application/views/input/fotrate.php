c11f486ba031a132b3dd9167d12b62b5cf8b1469
rezadyk <safri.110798@gmail.com>
2020-09-08 16:14:49 +0700

    merapikan tampilan

--- application/views/input/fotrate.php
+++ application/views/input/fotrate.php
@@ -4,7 +4,7 @@
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
     <div class="row">
-        <div class="col-lg-8">
+        <div class="col-lg-10">
 
             <div class="form-group row">
                 <label for="nik" class="col-sm-2 col-form-label">Nomor Tiket</label>
@@ -22,27 +22,21 @@
                     </div>
                 </div>
             </div>
-            <div class="form-group row">
-                <div class="col-sm-2">URL Rating</div>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="url" name="url" value="<?= base_url('/rating/index/') . $penilaian['token']; ?>" readonly>
-                    <button onclick="myFunction()" onmouseout="outFunc()" class="btn btn-secondary">
-                        <span class="tooltiptext" id="myTooltip">Copy to clipboard</span>
-                    </button>
-                    <div class="col-sm-10 mt-2">
-                        <span class="heading">Rating Konsumen</span>
-                        <?php for ($i = 0; $i < 5; $i++) : ?>
-                            <?php if ($i >= $penilaian['rate']) : ?>
-                                <?= '<span class="fa fa-star"></span>'; ?>
-                            <?php else : ?>
-                                <?= '<span class="fa fa-star" style="color: orange;" ></span>'; ?>
-                            <?php endif; ?>
-                        <?php endfor; ?>
-                        <textarea class="form-control" readonly value=""><?= $penilaian['keterangan']; ?></textarea>
-                    </div>
 
+            <div class="form-group row">
+                <label for="nik" class="col-sm-2 col-form-label">Rating Konsumen</label>
+                <div class="col-sm-10">
+                    <?php for ($i = 0; $i < 5; $i++) : ?>
+                        <?php if ($i >= $penilaian['rate']) : ?>
+                            <?= '<span class="fa fa-star"></span>'; ?>
+                        <?php else : ?>
+                            <?= '<span class="fa fa-star" style="color: orange;" ></span>'; ?>
+                        <?php endif; ?>
+                    <?php endfor; ?>
+                    <textarea class="form-control" readonly value=""><?= $penilaian['keterangan']; ?></textarea>
                 </div>
             </div>
+
 
         </div>
     </div>
